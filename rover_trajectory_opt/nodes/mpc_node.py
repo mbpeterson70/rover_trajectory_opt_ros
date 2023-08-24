fix rotation bug in mpc node

rover_trajectory_opt/nodes/mpc_node.py
@@ -75,8 +75,8 @@ class ModelPredictiveControlNode():
         self.state[0] = pose_stamped.pose.position.x
         self.state[1] = pose_stamped.pose.position.y
         quat = pose_stamped.pose.orientation
-        theta_unwrapped = Rot.from_quat([quat.x, quat.y, quat.w, quat.z]).as_euler('xyz')[2] + np.pi # add pi because of how theta is defined in Dubins dynamis
-        self.state[3] = -((theta_unwrapped + np.pi) % (2 * np.pi) - np.pi) # wrap
+        theta_unwrapped = Rot.from_quat([quat.x, quat.y, quat.z, quat.w]).as_euler('xyz')[2]
+        self.state[3] = ((theta_unwrapped + np.pi) % (2 * np.pi) - np.pi) # wrap
         
     def twist_cb(self, twist_stamped):
         theta = self.state[3]